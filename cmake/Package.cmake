d1faef883685ce7cf65df4e610827d9554af7833
John <yangtianzhuo970425@gmail.com>
2024-12-13 21:08:38 +1300

    Bump project version to 0.3.0, enhance package handling, and improve dependency messages

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -12,36 +12,77 @@
 
 MESSAGE(STATUS "Include Package.cmake")
 
+SET(_PACKAGE_HAS_DEPENDENCIES 0)
+
+FUNCTION(TO_PACKAGE_NAME RST NAME VERSION)
+    SET(TMP "${NAME}_${VERSION}")
+    STRING(REPLACE "." "_" TMP ${TMP})
+    SET(${RST} ${TMP} PARENT_SCOPE)
+ENDFUNCTION()
+
 FUNCTION(PACKAGE_NAME RST)
-    SET(${RST} "${PROJECT_NAME}-${PROJECT_VERSION}" PARENT_SCOPE)
+    TO_PACKAGE_NAME(TMP ${PROJECT_NAME} ${PROJECT_VERSION})
+    SET(${RST} ${TMP} PARENT_SCOPE)
 ENDFUNCTION()
 
 MACRO(ADD_DEP NAME VERSION)
-    MESSAGE(STATUS "Finding ${NAME}-${VERSION}...")
+    SET(_PACKAGE_HAS_DEPENDENCIES 1)
+    LIST(APPEND _PACKAGE_DEP_NAME_LIST ${NAME})
+    LIST(APPEND _PACKAGE_DEP_VERSION_LIST ${VERSION})
+    MESSAGE(STATUS "Finding dependency: ${NAME} v${VERSION}")
     FIND_PACKAGE(${NAME} ${VERSION} QUIET)
     IF (${${NAME}_FOUND})
-        MESSAGE(STATUS "${NAME} found: ${NAME}-${VERSION}")
+        MESSAGE(STATUS "${NAME} found: ${NAME} v${VERSION}")
     ELSE ()
         SET(ADDRESS "https://github.com/shimakaze09/${NAME}")
-        MESSAGE(STATUS "${NAME}-${VERSION} not found, fetching from ${ADDRESS}\n"
+        MESSAGE(STATUS "${NAME} v${VERSION} not found, fetching from ${ADDRESS}\n"
                 "Please wait for a while...")
         FETCHCONTENT_DECLARE(
                 ${NAME}
-                GIT_REPOSITORY "https://github.com/shimakaze09/${NAME}.git"
+                GIT_REPOSITORY ${ADDRESS}
                 GIT_TAG "v${VERSION}"
         )
-        MESSAGE(STATUS "Building ${NAME}-${VERSION}...")
+        MESSAGE(STATUS "Building ${NAME} v${VERSION}...")
         FETCHCONTENT_MAKEAVAILABLE(${NAME})
-        MESSAGE(STATUS "${NAME} built: ${NAME}-${VERSION}")
+        MESSAGE(STATUS "${NAME} built: ${NAME} v${VERSION}")
     ENDIF ()
 ENDMACRO()
 
 MACRO(EXPORT_TARGETS)
-    CMAKE_PARSE_ARGUMENTS("ARG" "" "INC;TARGET" "" ${ARGN})
+    CMAKE_PARSE_ARGUMENTS("ARG" "" "TARGET" "DIRECTORIES" "" ${ARGN})
 
     PACKAGE_NAME(PACKAGE_NAME)
     MESSAGE(STATUS "${PACKAGE_NAME}")
     MESSAGE(STATUS "Exporting ${PACKAGE_NAME}")
+
+    IF (${_PACKAGE_HAS_DEPENDENCIES})
+        SET(MY_PACKAGE_INIT "MESSAGE(STATUS \"Finding package: MyCMake v0.3.0\")
+                          FIND_PACKAGE(MyCMake 0.3.0 QUIET)
+                          IF (\${MyCMake_FOUND})
+                                MESSAGE(STATUS \"MyCMake found: MyCMake ${MyCMake_VERSION}\")
+                          ELSE ()
+                                SET(ADDRESS \"https://github.com/shimakaze09/MyCMake\")
+                                MESSAGE(STATUS \"MyCMake v0.3.0 not found, fetching from \${ADDRESS}\n\"
+                                                \"Please wait for a while...\")
+                                FETCHCONTENT_DECLARE(
+                                        MyCMake
+                                        GIT_REPOSITORY \${ADDRESS}
+                                        GIT_TAG \"v0.3.0\"
+                                )
+                                MESSAGE(STATUS \"Building MyCMake v0.3.0...\")
+                                FETCHCONTENT_MAKEAVAILABLE(MyCMake)
+                                MESSAGE(STATUS \"MyCMake built: MyCMake v0.3.0\")
+                            ENDIF ()")
+        MESSAGE(STATUS "[Dependencies]")
+        LIST(LENGTH _PACKAGE_DEP_NAME_LIST _PACKAGE_DEP_NUM)
+        MATH(EXPR _PACKAGE_STOP "${_PACKAGE_DEP_NUM} - 1")
+        FOREACH (INDEX RANGE ${_PACKAGE_STOP})
+            LIST(GET _PACKAGE_DEP_NAME_LIST ${INDEX} DEP_NAME)
+            LIST(GET _PACKAGE_DEP_VERSION_LIST ${INDEX} DEP_VERSION)
+            MESSAGE(STATUS "  ${DEP_NAME} v${DEP_VERSION}")
+            SET(MY_PACKAGE_INIT "${MY_PACKAGE_INIT}\nADD_DEP(${DEP_NAME} ${DEP_VERSION})")
+        ENDFOREACH ()
+    ENDIF ()
 
     IF (NOT "${ARG_TARGET}" STREQUAL "OFF")
         # Generate the export targets for the build tree
@@ -82,7 +123,7 @@ MACRO(EXPORT_TARGETS)
             DESTINATION "${package_name}/cmake"
     )
 
-    IF (NOT "${ARG_INC}" STREQUAL "OFF")
-        INSTALL(DIRECTORY "include" DESTINATION ${PACKAGE_NAME})
-    ENDIF ()
+    FOREACH (DIR ${ARG_DIRECTORIES})
+        INSTALL(DIRECTORY ${DIR} DESTINATION ${PACKAGE_NAME})
+    ENDFOREACH ()
 ENDMACRO()
