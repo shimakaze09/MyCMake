766ce91efa91647589864021f53bca75be8f4e9e
John <yangtianzhuo970425@gmail.com>
2024-12-14 22:20:17 +1300

    Refactor CMake macros for improved dependency handling and export functionality

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -27,63 +27,64 @@ ENDFUNCTION()
 
 MACRO(ADD_DEP NAME VERSION)
     SET(_PACKAGE_HAS_DEPENDENCIES 1)
-    LIST(APPEND _PACKAGE_DEP_NAME_LIST ${NAME})
-    LIST(APPEND _PACKAGE_DEP_VERSION_LIST ${VERSION})
+    LIST(APPEND PACKAGE_DEP_NAME_LIST ${NAME})
+    LIST(APPEND PACKAGE_DEP_VERSION_LIST ${VERSION})
     MESSAGE(STATUS "Finding dependency: ${NAME} v${VERSION}")
     FIND_PACKAGE(${NAME} ${VERSION} QUIET)
     IF (${${NAME}_FOUND})
-        MESSAGE(STATUS "${NAME} found: ${NAME} v${VERSION}")
-    ELSE ()
-        SET(ADDRESS "https://github.com/shimakaze09/${NAME}")
-        MESSAGE(STATUS "${NAME} v${VERSION} not found, fetching from ${ADDRESS}\n"
-                "Please wait for a while...")
-        FETCHCONTENT_DECLARE(
+        MESSAGE(STATUS "${NAME} v${${NAME}_VERSION} found")
+    else ()
+        SET(ADDRESS "https://github.com/shimakaze09/${NAME}.git")
+        MESSAGE(STATUS "${NAME} v${VERSION} not found, fetching ...\n"
+                "Fetch from: ${ADDRESS} with tag v${version}")
+        FetchContent_Declare(
                 ${NAME}
                 GIT_REPOSITORY ${ADDRESS}
                 GIT_TAG "v${VERSION}"
         )
         MESSAGE(STATUS "Building ${NAME} v${VERSION}...")
         FETCHCONTENT_MAKEAVAILABLE(${NAME})
-        MESSAGE(STATUS "${NAME} built: ${NAME} v${VERSION}")
+        MESSAGE(STATUS "Built: ${NAME} v${VERSION}")
     ENDIF ()
 ENDMACRO()
 
-MACRO(EXPORT_TARGETS)
-    CMAKE_PARSE_ARGUMENTS("ARG" "" "TARGET" "DIRECTORIES" "" ${ARGN})
+MACRO(MY_EXPORT)
+    CMAKE_PARSE_ARGUMENTS("ARG" "" "TARGET" "DIRECTORIES" ${ARGN})
 
-    PACKAGE_NAME(PACKAGE_NAME)
-    MESSAGE(STATUS "${PACKAGE_NAME}")
-    MESSAGE(STATUS "Exporting ${PACKAGE_NAME}")
+    PACKAGE_NAME(PKG_NAME)
+    MESSAGE(STATUS "${PKG_NAME}")
+    MESSAGE(STATUS "Exporting ${PKG_NAME}")
 
     IF (${_PACKAGE_HAS_DEPENDENCIES})
         SET(MY_PACKAGE_INIT "
-                          IF(NOT ${FETCHCONTENT_FOUND})
-                                INCLUDE(FetchContent)
-                          ENDIF ()
-                          MESSAGE(STATUS \"Finding package: MyCMake v0.3.0\")
-                          FIND_PACKAGE(MyCMake 0.3.0 QUIET)
-                          IF (\${MyCMake_FOUND})
-                                MESSAGE(STATUS \"MyCMake found: MyCMake v\${MyCMake_VERSION}\")
-                          ELSE ()
-                                SET(ADDRESS \"https://github.com/shimakaze09/MyCMake\")
-                                MESSAGE(STATUS \"MyCMake v0.3.0 not found, fetching from \${ADDRESS}\n\"
-                                                \"Please wait for a while...\")
-                                FETCHCONTENT_DECLARE(
-                                        MyCMake
-                                        GIT_REPOSITORY \${ADDRESS}
-                                        GIT_TAG \"v0.3.0\"
-                                )
-                                MESSAGE(STATUS \"Building MyCMake v0.3.0...\")
-                                FETCHCONTENT_MAKEAVAILABLE(MyCMake)
-                                MESSAGE(STATUS \"MyCMake built: MyCMake v0.3.0\")
-                          ENDIF ()")
+IF (NOT ${FetchContent_FOUND})
+	INCLUDE(FetchContent)
+ENDIF ()
+MESSAGE (STATUS \"find package: MyCMake v0.3.4\")
+FIND_PACKAGE(MyCMake 0.3.4 QUIET)
+IF(\${MyCMake_FOUND})
+	MESSAGE(STATUS \"MyCMake v\${MyCMake_VERSION} found\")
+ELSE()
+	SET(PACKAGE_ADDRESS \"https://github.com/shimakaze09/MyCMake.git\")
+	MESSAGE(STATUS \"UCMake v0.3.0 not found, so fetch it ...\")
+	MESSAGE(STATUS \"fetch: \${PACKAGE_ADDRESS} with tag v0.3.4\")
+	FETCHCONTENT_DECLARE(
+	  MyCMake
+	  GIT_REPOSITORY \${PACKAGE_ADDRESS}
+	  GIT_TAG \"v0.3.4\"
+	)
+	MESSAGE(STATUS \"MyCMake v0.3.4 fetch done, building ...\")
+	FETCHCONTENT_MAKEAVAILABLE(MyCMake)
+	MESSAGE(STATUS \"MyCMake v0.3.4 build done\")
+ENDIF()
+")
         MESSAGE(STATUS "[Dependencies]")
         LIST(LENGTH _PACKAGE_DEP_NAME_LIST _PACKAGE_DEP_NUM)
-        MATH(EXPR _PACKAGE_STOP "${_PACKAGE_DEP_NUM} - 1")
+        MATH(EXPR _PACKAGE_STOP "${_PACKAGE_DEP_NUM}-1")
         FOREACH (INDEX RANGE ${_PACKAGE_STOP})
             LIST(GET _PACKAGE_DEP_NAME_LIST ${INDEX} DEP_NAME)
             LIST(GET _PACKAGE_DEP_VERSION_LIST ${INDEX} DEP_VERSION)
-            MESSAGE(STATUS "  ${DEP_NAME} v${DEP_VERSION}")
+            MESSAGE(STATUS "- ${DEP_NAME} v${DEP_VERSION}")
             SET(MY_PACKAGE_INIT "${MY_PACKAGE_INIT}\nADD_DEP(${DEP_NAME} ${DEP_VERSION})")
         ENDFOREACH ()
     ENDIF ()
@@ -100,16 +101,15 @@ MACRO(EXPORT_TARGETS)
         INSTALL(EXPORT "${PROJECT_NAME}Targets"
                 FILE "${PROJECT_NAME}Targets.cmake"
                 NAMESPACE "My::"
-                DESTINATION "${PACKAGE_NAME}/cmake"
+                DESTINATION "${PKG_NAME}/cmake"
         )
     ENDIF ()
 
     INCLUDE(CMakePackageConfigHelpers)
-
     # Generate the config file that is includes the exports
     CONFIGURE_PACKAGE_CONFIG_FILE(${PROJECT_SOURCE_DIR}/config/Config.cmake.in
             "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
-            INSTALL_DESTINATION "${PACKAGE_NAME}/cmake"
+            INSTALL_DESTINATION "${PKG_NAME}/cmake"
             NO_SET_AND_CHECK_MACRO
             NO_CHECK_REQUIRED_COMPONENTS_MACRO
     )
@@ -129,6 +129,6 @@ MACRO(EXPORT_TARGETS)
     )
 
     FOREACH (DIR ${ARG_DIRECTORIES})
-        INSTALL(DIRECTORY ${DIR} DESTINATION ${PACKAGE_NAME})
+        INSTALL(DIRECTORY ${DIR} DESTINATION ${PKG_NAME})
     ENDFOREACH ()
 ENDMACRO()
