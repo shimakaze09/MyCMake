8e1f5b0767a21c7bf30a55e18effe501b458c8a4
John <yangtianzhuo970425@gmail.com>
2025-02-23 10:47:48 +1300

    Bump project version to 0.4.1 and add Download.cmake for file downloading functionality

--- cmake/Tools.cmake
+++ cmake/Tools.cmake
@@ -2,3 +2,4 @@ MESSAGE(STATUS "Include Tools.cmake")
 
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Git.cmake")
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Build.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Download.cmake")
