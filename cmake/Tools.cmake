b57709d7f36b0f5e35f2c4e58d71bc49399f237d
John <yangtianzhuo970425@gmail.com>
2025-03-10 18:45:01 +1300

    Refactor CMake initialization and packaging scripts; remove Tools.cmake and improve message formatting

--- cmake/Tools.cmake
+++
@@ -1,5 +0,0 @@
-MESSAGE(STATUS "Include Tools.cmake")
-
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Git.cmake")
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Build.cmake")
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Download.cmake")
